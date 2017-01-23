e56995b18c5cdcc0207e02ece42d3894ed2da3ed
Justin Boundy <jboundy@advancedtech.com>
2017-01-23 15:15:58 -0600

    Added server for creating crud operations with firebase

---
+++ Secret.Santa/Services/SecretSantaService.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Firebase.Database;
+using Secret.Santa.Settings;
+
+namespace Secret.Santa.Services
+{
+    public class SecretSantaService
+    {
+        private FirebaseClient _firebaseClient;
+        public SecretSantaService(FirebaseConfiguration config)
+        {
+            _firebaseClient = new FirebaseClient(config.AuthDomain, new FirebaseOptions
+            {
+                AuthTokenAsyncFactory = () => Task.FromResult(config.ApiKey)
+            });
+        }
+    }
+}
