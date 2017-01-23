7dc413d546fa03fe5e6fc1babceb3f931be22764
Justin Boundy <jboundy@advancedtech.com>
2017-01-23 08:57:46 -0600

    initial commit

---
+++ Secret.Santa/Settings/FirebaseConfiguration.cs
@@ -0,0 +1,11 @@
+﻿namespace Secret.Santa.Settings
+{
+    public class FirebaseConfiguration
+    {
+        public string ApiKey { get; set; }
+        public string AuthDomain { get; set; }
+        public string DatabaseUrl { get; set; }
+        public string StorageBucket { get; set; }
+        public string MessagingSenderId { get; set; }
+    }
+}
